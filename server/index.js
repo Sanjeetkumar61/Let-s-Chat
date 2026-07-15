fbd0ac198c16ad6eb088df9b371fb138aecc6bc0
Sanjeetkumar61 <sanjeetv090@gmail.com>
2026-07-15 14:15:35 +0530

    update sockets cors and origin

--- server/index.js
+++ server/index.js
@@ -22,7 +22,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173",
+      process.env.CLIENT_URL],
     credentials: true,
   },
 });
@@ -33,8 +34,10 @@ socketHandler(io);
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173",
+      process.env.CLIENT_URL],
     credentials: true,
+
   })
 );
 
