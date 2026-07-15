5e1851f95d76f7915275afc2cda25f7bb0d5cc33
Sanjeetkumar61 <sanjeetv090@gmail.com>
2026-07-15 12:20:41 +0530

    Rename user model temporarily

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,39 @@
-import { Schema, model } from "mongoose"
+// import { Schema, model } from "mongoose"
 
-const userSchemma = new Schema(
+// const userSchemma = new Schema(
+//   {
+//     name: {
+//       type: String,
+//       required: [true, "Name is required"],
+//       trim: true,
+//     },
+
+//     email: {
+//       type: String,
+//       required: [true, "Email is required"],
+//       unique: true,
+//       lowercase: true,
+//       trim: true,
+//     },
+
+//     password: {
+//       type: String,
+//       required: [true, "Password is required"],
+//       minlength: 6,
+//     },
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+// const User = model("User", userSchemma);
+
+// export default User;
+
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
@@ -27,6 +60,4 @@ const userSchemma = new Schema(
   }
 );
 
-const User = model("User", userSchemma);
-
-export default User;
+export default mongoose.models.User || mongoose.model("User", userSchema);
